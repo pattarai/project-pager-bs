f08a429b4056a1173fbc4221c6595c00e5abcd23
veroni-d20 <veronishwetha.23it@licet.ac.in>
2021-10-18 21:38:56 +0530

    message endpoint for discord

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
-from flask import Flask
+from flask import Flask, request
 from flask.helpers import send_from_directory
-import os
+import os, requests, json
 
 app = Flask(__name__, static_url_path='', static_folder='.')
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 # CORS section
-
-
 @app.after_request
 def after_request_func(response):
     response.headers.add("Access-Control-Allow-Origin", "*")
@@ -16,14 +14,12 @@ def after_request_func(response):
     return response
 # end CORS section
 
-
 import error_handles
 
 # Add your API endpoints here
 from routes import users
 # from routes import cars
 # ...
-
 
 @app.route('/')
 def get_endpoint_function():
@@ -34,6 +30,23 @@ def get_endpoint_function():
     except Exception as e:
         print(e)
 
+# Send message to discord
+@app.route('/message', methods=['GET'])
+def send_message():
+    try:
+        message = request.args["message"]        
+        url = "https://discord.com/api/webhooks/890522840821006366/Ol38KeLTe2aXel4y2ECnDLuiDdilNYiEjrhMYHTKp8FsGyVPn6CFHD5HwM-7nCR8ww0B"
+        payload = json.dumps({
+        "content": message
+        })
+        headers = {
+        'Content-Type': 'application/json',
+        'Cookie': '__cfruid=b882f9b808096b92c166f27597787a81fff7a4cf-1632389271; __dcfduid=88e1af7c1c5011ecbc9842010a0a066c; __sdcfduid=88e1af7c1c5011ecbc9842010a0a066c8b54e6b16757ef5e34f069e7077c55d22b13cb99c9f9a5a17bafc3d214f0e507'
+        }
+        response = requests.request("POST", url, headers=headers, data=payload)
+        return "success"
+    except Exception as e:
+        print(e)
 
 # Setting Favicon
 @app.route('/favicon.ico')
